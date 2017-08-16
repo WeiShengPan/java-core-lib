11e0b1eb46b306edaaf4e93294911bd59d277843
panws <panws@wangsu.com>
2017-08-16 18:35:54 +0800

    Add some java nio features

--- src/main/java/javafeatures/iotest/nio/FileChannelTest.java
+++ src/main/java/javafeatures/iotest/nio/FileChannelTest.java
@@ -20,16 +20,18 @@ public class FileChannelTest {
 
 	private static final int B_SIZE = 1024;
 
+	private static final String FILE_PATH = "src/main/resources/io/output5.txt";
+
 	public static void main(String[] args) throws IOException {
 
 		//write from FileOutputStream
-		try (FileChannel fc = new FileOutputStream("src/main/resources/io/output5.txt").getChannel()) {
+		try (FileChannel fc = new FileOutputStream(FILE_PATH).getChannel()) {
 
 			fc.write(ByteBuffer.wrap("你好 \n".getBytes()));
 		}
 
 		//write from RandomAccessFile
-		try (FileChannel fc = new RandomAccessFile("src/main/resources/io/output5.txt", "rw").getChannel()) {
+		try (FileChannel fc = new RandomAccessFile(FILE_PATH, "rw").getChannel()) {
 
 			fc.position(fc.size());    //move to the end of the file
 
@@ -37,7 +39,7 @@ public class FileChannelTest {
 		}
 
 		//read from FileInputStream
-		try (FileChannel fc = new FileInputStream("src/main/resources/io/output5.txt").getChannel()) {
+		try (FileChannel fc = new FileInputStream(FILE_PATH).getChannel()) {
 
 			//只读访问必须分配ByteBuffer的大小，更高速度可用ByteBuffer.allocateDirect()产生直接缓冲器，但开支更大
 			ByteBuffer byteBuffer = ByteBuffer.allocate(B_SIZE);
