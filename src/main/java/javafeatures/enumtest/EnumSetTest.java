97fa1c2da82ed8c3a60688792bd2d2eb07e12eec
panws <panws@wangsu.com>
2017-08-18 17:35:39 +0800

    Add some java enum features

---
+++ src/main/java/javafeatures/enumtest/EnumSetTest.java
@@ -0,0 +1,38 @@
+package javafeatures.enumtest;
+
+import java.util.EnumSet;
+
+/**
+ * @author panws
+ * @since 2017-08-18
+ */
+public class EnumSetTest {
+
+	public static void main(String[] args) {
+
+		EnumSet<EnumWeek> weekEnumSet = EnumSet.noneOf(EnumWeek.class);
+		System.out.println(weekEnumSet);
+
+		weekEnumSet.add(EnumWeek.MON);
+		System.out.println(weekEnumSet);
+
+		weekEnumSet.addAll(EnumSet.allOf(EnumWeek.class));
+		System.out.println(weekEnumSet);
+
+		weekEnumSet.removeAll(EnumSet.of(EnumWeek.TUE, EnumWeek.WED));
+		System.out.println(weekEnumSet);
+
+		weekEnumSet.removeAll(EnumSet.range(EnumWeek.FRI, EnumWeek.SUN));
+		System.out.println(weekEnumSet);
+
+		weekEnumSet = EnumSet.allOf(EnumWeek.class);
+		System.out.println(weekEnumSet);
+
+		weekEnumSet.remove(EnumWeek.MON);
+		System.out.println(weekEnumSet);
+
+		System.out.println(weekEnumSet.contains(EnumWeek.MON));
+		System.out.println(weekEnumSet.contains(EnumWeek.TUE));
+
+	}
+}
---
+++ src/main/java/javafeatures/enumtest/RandomEnumTest.java
@@ -0,0 +1,29 @@
+package javafeatures.enumtest;
+
+import java.util.Random;
+
+/**
+ * 随机返回枚举类中的任意一个实例
+ *
+ * @author panws
+ * @since 2017-08-18
+ */
+public class RandomEnumTest {
+
+	private static Random rand = new Random(47);
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 7; i++) {
+			System.out.println(random(EnumWeek.class));
+		}
+	}
+
+	public static <T extends Enum<T>> T random(Class<T> ec) {
+		return random(ec.getEnumConstants());
+	}
+
+	public static <T> T random(T[] values) {
+		return values[rand.nextInt(values.length)];
+	}
+}
