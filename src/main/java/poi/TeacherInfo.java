052ae8879164ff396b1f073d3b915153379e8b0f
WeishengPan <pws522@qq.com>
2020-03-15 13:12:16 +0800

    poi

---
+++ src/main/java/poi/TeacherInfo.java
@@ -0,0 +1,26 @@
+package poi;
+
+import java.util.Map;
+
+public class TeacherInfo {
+
+    private String date;
+
+    private Map<String, Integer> teacherMap;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Map<String, Integer> getTeacherMap() {
+        return teacherMap;
+    }
+
+    public void setTeacherMap(Map<String, Integer> teacherMap) {
+        this.teacherMap = teacherMap;
+    }
+}
