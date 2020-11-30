facce92040ad47d4aa51f1ccd3467baaa0ffe390
wilson.pan <wilson.pan@ringcentral.com>
2020-11-30 17:23:20 +0800

    Spring health indicator

---
+++ src/main/java/com/pws/spring/RandomHealthIndicator.java
@@ -0,0 +1,26 @@
+package com.pws.spring;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * /management/health
+ *
+ * @author wilson.pan
+ * @date 2020/11/30
+ */
+@Component
+public class RandomHealthIndicator extends AbstractHealthIndicator {
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        double chance = ThreadLocalRandom.current().nextDouble();
+        builder.up();
+        if (chance > 0.9) {
+            builder.down();
+        }
+    }
+}
