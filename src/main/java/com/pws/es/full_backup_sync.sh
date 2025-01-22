7a9e1c80870f48ad59544dcc3816024fe227a4ea
潘玮晟 <pweisheng@cndinfo.com>
2025-01-22 11:08:36 +0800

    add es snapshot and restore bash script

--- src/main/java/com/pws/es/full_backup_sync.sh
+++ src/main/java/com/pws/es/full_backup_sync.sh
@@ -1,7 +1,7 @@
 #!/opt/homebrew/bin/bash
 
 #######
-# ES索引增量备份，仅支持从prod备份到test环境
+# ES索引索引备份，仅支持从prod备份到test环境
 # 支持索引：[1] dps-power-station-device-history-log, [2] dps-alarm-device-rule-record-log
 # 支持模式：[1] snapshot&restore, [2] snapshot only, [3] restore only
 ##
@@ -26,10 +26,12 @@ ES_TARGET_HOST="xxx"  # TODO: replace me
 SNAPSHOT_REPO="es_backup"
 
 # index and snapshot definition
-SOURCE_INDICES=("dps-power-station-device-history-log" "dps-alarm-device-rule-record-log")
+SOURCE_INDICES=("dps-power-station-device-history-log" "dps-alarm-device-rule-record-log" "dps-power-station-device-month-electric-log" "dps-power-station-device-year-electric-log")
 declare -A SNAPSHOT_NAMES_MAP=(
-  ["dps-power-station-device-history-log"]="snapshot_device_history_log_increment_temp"
-  ["dps-alarm-device-rule-record-log"]="snapshot_alarm_log_increment_temp"
+  ["dps-power-station-device-history-log"]="snapshot_device_history_detail_log_full_temp"
+  ["dps-alarm-device-rule-record-log"]="snapshot_alarm_log_full_temp"
+  ["dps-power-station-device-month-electric-log"]="snapshot_device_history_month_log_full_temp"
+  ["dps-power-station-device-year-electric-log"]="snapshot_device_history_year_log_full_temp"
 )
 
 # reindex task id
@@ -118,14 +120,6 @@ execute() {
   echo "$response"
 }
 
-check_if_restore() {
-  read -p "请确认是否继续执行目标集群增量恢复步骤 (输入 'yes' 继续): " USER_INPUT
-  if [ "$USER_INPUT" != "yes" ]; then
-    echo "操作已终止。"
-    exit 0
-  fi
-}
-
 check_continue() {
   echo ""
   read -p "请确认是否继续 (输入 'yes' 继续): " USER_INPUT
@@ -145,14 +139,12 @@ choose_source_index() {
   read -p "请输入索引对应的编号 (1-${#SOURCE_INDICES[@]}): " INDEX_CHOICE
   if [[ "$INDEX_CHOICE" -ge 1 && "$INDEX_CHOICE" -le ${#SOURCE_INDICES[@]} ]]; then
     SOURCE_INDEX="${SOURCE_INDICES[$((INDEX_CHOICE - 1))]}"
-    TEMP_INDEX="${SOURCE_INDEX}-temp"
     # TODO: change me
     TARGET_INDEX="${SOURCE_INDEX}-prod"
 #    TARGET_INDEX="${SOURCE_INDEX}"
     SNAPSHOT_NAME=${SNAPSHOT_NAMES_MAP[$SOURCE_INDEX]}
     echo "已选择："
     echo "- 源索引：$SOURCE_INDEX"
-    echo "- 临时索引：$TEMP_INDEX"
     echo "- 目标索引：$TARGET_INDEX"
     echo "- 快照名称：$SNAPSHOT_NAME"
   else
@@ -212,121 +204,6 @@ list_snapshots() {
   check_continue
 }
 
-# 创建临时索引并执行增量 Reindex
-create_temp_index() {
-  local es_host=$1
-  local es_credentials
-  es_credentials=$(get_es_credentials "$es_host")
-  local es_req_url="$es_host/_reindex?wait_for_completion=false"
-
-  echo_color 3 "\n#### 请指定增量数据的时间范围，输入最近几天的数据 (如输入 3 表示最近 3 天):"
-  read -p "请输入天数: " DAYS
-  if ! [[ "$DAYS" =~ ^[0-9]+$ ]]; then
-    echo "输入无效，请输入正整数天数！"
-    exit 1
-  fi
-
-  echo "创建临时索引并执行增量数据复制，时间范围为最近 $DAYS 天..."
-
-  local time_field="createTime"
-  [ "$SOURCE_INDEX" == "dps-alarm-device-rule-record-log" ] && time_field="sCreateTime"
-
-  local es_req_body='{
-              "source": {
-                "index": "'"$SOURCE_INDEX"'",
-                "query": {
-                  "range": {
-                    "'"$time_field"'": {
-                      "gte": "now-'$DAYS'd",
-                      "lt": "now"
-                    }
-                  }
-                }
-              },
-              "dest": {
-                "index": "'"$TEMP_INDEX"'"
-              }
-            }'
-
-  echo_color 4 "API 请求 [GET]: $es_req_url"
-  echo_color 4 "请求数据: $es_req_body"
-  RESPONSE=$(execute "POST" "$es_req_url" "$es_req_body" "$es_credentials")
-  echo "API 响应:"
-  echo "$RESPONSE" | jq . 2>/dev/null || echo "$RESPONSE"
-
-  # 提取任务 ID
-  SOURCE_REINDEX_TASK_ID=$(echo "$RESPONSE" | jq -r '.task')
-  if [ -z "$SOURCE_REINDEX_TASK_ID" ] || [ "$SOURCE_REINDEX_TASK_ID" == "null" ]; then
-    echo "无法提取任务 ID，退出。"
-    exit 1
-  fi
-  echo "Reindex 任务已提交，任务 ID: $SOURCE_REINDEX_TASK_ID"
-}
-
-# 将临时index结果复制到目标index
-copy_temp_index_to_target_index() {
-  local es_host=$1
-  local es_credentials
-  es_credentials=$(get_es_credentials "$es_host")
-  local es_req_url="$es_host/_reindex?wait_for_completion=false"
-  local es_req_body='{
-        "source": {
-          "index": "'"$TEMP_INDEX"'"
-        },
-        "dest": {
-          "index": "'"$TARGET_INDEX"'"
-        }
-      }'
-
-  echo_color 3 "\n#### 恢复临时索引数据到目标索引..."
-
-  echo_color 4 "API 请求 [GET]: $es_req_url"
-  echo_color 4 "请求数据: $es_req_body"
-  RESPONSE=$(execute "POST" "$es_req_url" "$es_req_body" "$es_credentials")
-
-  # 提取任务 ID
-  TARGET_REINDEX_TASK_ID=$(echo "$RESPONSE" | jq -r '.task')
-  if [ -z "$TARGET_REINDEX_TASK_ID" ] || [ "$TARGET_REINDEX_TASK_ID" == "null" ]; then
-    echo "无法提取任务 ID，退出。"
-    exit 1
-  fi
-  echo "Reindex 任务已提交，任务 ID: $TARGET_REINDEX_TASK_ID"
-}
-
-# 循环检查 Reindex 状态
-wait_for_reindex_completion() {
-  local es_host=$1
-  local es_credentials
-  es_credentials=$(get_es_credentials "$es_host")
-  local task_id=$2
-  local url="$es_host/_tasks/$task_id"
-
-  echo_color 3 "\n#### 开始循环检查 Reindex 任务状态，任务 ID: $task_id"
-  while true; do
-    echo_color 4 "API 请求 [GET]: $url"
-    RESPONSE=$(curl -s -u "$es_credentials" -X GET "$url")
-    echo "当前任务状态:"
-    echo "$RESPONSE" | jq . 2>/dev/null || echo "$RESPONSE"
-
-    # 检查任务状态是否完成
-    COMPLETED=$(echo "$RESPONSE" | jq -r '.completed')
-    if [ "$COMPLETED" == "true" ]; then
-      echo "Reindex 任务已完成！"
-      break
-    else
-      echo "Reindex 尚未完成，等待 10 秒后重试..."
-      sleep 10
-    fi
-  done
-
-  # 等待用户确认继续
-  read -p "请确认是否继续执行后续步骤 (输入 'yes' 继续): " USER_INPUT
-  if [ "$USER_INPUT" != "yes" ]; then
-    echo "操作已终止。"
-    exit 0
-  fi
-}
-
 # 创建快照
 create_snapshot() {
   local es_host=$1
@@ -335,7 +212,7 @@ create_snapshot() {
 
   echo_color 3 "\n#### 为临时索引创建快照..."
   execute_and_print "PUT" "$es_host/_snapshot/$SNAPSHOT_REPO/$SNAPSHOT_NAME" '{
-    "indices": "'"$TEMP_INDEX"'"
+    "indices": "'"$SOURCE_INDEX"'"
   }' "$es_credentials"
 
   check_continue
@@ -377,9 +254,9 @@ wait_for_restore_completion() {
   local es_host=$1
   local credentials
   credentials=$(get_es_credentials "$es_host")
-  local es_req_url="$es_host/$TEMP_INDEX/_recovery"
+  local es_req_url="$es_host/$TARGET_INDEX/_recovery"
 
-  echo_color 3 "#### 开始监控索引 [$TEMP_INDEX] 的恢复进度..."
+  echo_color 3 "#### 开始监控索引 [$TARGET_INDEX] 的恢复进度..."
   while true; do
     # 查询恢复状态
     echo_color 4 "API 请求 [GET]: $es_req_url"
@@ -409,8 +286,6 @@ wait_for_restore_completion() {
     # 等待 5 秒后再次检查
     sleep 5
   done
-
-  check_continue
 }
 
 # 恢复快照
@@ -421,16 +296,62 @@ perform_restore() {
 
   echo_color 3 "\n#### 执行快照恢复，恢复至临时索引..."
   execute_and_print "POST" "$es_host/_snapshot/$SNAPSHOT_REPO/$SNAPSHOT_NAME/_restore" '{
-    "indices": "'"$TEMP_INDEX"'",
-    "rename_pattern": "'"$TEMP_INDEX"'",
-    "rename_replacement": "'"$TEMP_INDEX"'"
+    "indices": "'"$SOURCE_INDEX"'",
+    "rename_pattern": "'"$SOURCE_INDEX"'",
+    "rename_replacement": "'"$TARGET_INDEX"'"
   }' "$es_credentials"
 
   check_continue
 }
 
-# 删除临时索引
-delete_temp_index() {
+# 检查目标集群是否存在目标索引
+check_and_delete_target_index() {
+  local es_host=$1
+  local credentials
+  credentials=$(get_es_credentials "$es_host")
+  local es_req_url="$es_host/_cat/indices/$TARGET_INDEX"
+
+  while true; do
+    echo_color 3 "#### 检查索引是否存在：$TARGET_INDEX"
+
+    # 使用 HEAD 请求检查索引是否存在
+    echo_color 4 "API 请求 [GET]: $es_req_url"
+    local response
+#    response=$(curl -s -u "$credentials" -X GET "$es_req_url" | grep -w "$index_name")
+    response=$(curl -s -u "$credentials" -X GET "$es_req_url")
+    echo "$response" | jq . 2>/dev/null || echo "$response"
+
+    INDEX_EXIST=$(echo "$response" | grep -w "$TARGET_INDEX" | grep -w "green" )
+    echo "INDEX_EXIST: $INDEX_EXIST"
+    RESP_STATUS_CODE=$(echo "$response" | jq -r '.status' 2>/dev/null || echo "")
+    echo "RESP_STATUS_CODE: $RESP_STATUS_CODE"
+
+    if [[ -n "$RESP_STATUS_CODE" && "$RESP_STATUS_CODE" -eq 404 ]]; then
+      echo_color 2 "索引 [$TARGET_INDEX] 不存在，可以继续后续操作。"
+      break
+    fi
+
+    if [[ -n "$INDEX_EXIST" ]]; then
+      echo_color 2 "索引 [$TARGET_INDEX] 存在。"
+      echo_color 3 "请手动删除索引 [$TARGET_INDEX] 后回到脚本中重试。"
+      read -p "是否重新检查索引 [$TARGET_INDEX] 是否存在？(yes/no): " retry_check
+      if [ "$retry_check" != "yes" ]; then
+        echo_color 1 "用户选择退出检查，脚本终止。"
+        exit 1
+      fi
+      continue
+    fi
+
+    echo_color 1 "Error: 检查索引 [$TARGET_INDEX] 时发生错误，HTTP 响应为：$response"
+    exit 1
+
+  done
+
+  check_continue
+}
+
+# 删除已存在的目标索引
+delete_index() {
   local es_host=$1
   local es_credentials
   es_credentials=$(get_es_credentials "$es_host")
@@ -443,22 +364,17 @@ proc_snapshot() {
   echo_color 3 "开始执行增量备份流程..."
   delete_existing_snapshot "$ES_SOURCE_HOST"
   list_snapshots "$ES_SOURCE_HOST"
-  create_temp_index "$ES_SOURCE_HOST"
-  wait_for_reindex_completion "$ES_SOURCE_HOST" "$SOURCE_REINDEX_TASK_ID"
   create_snapshot "$ES_SOURCE_HOST"
   wait_for_snapshot_completion "$ES_SOURCE_HOST"
-  delete_temp_index "$ES_SOURCE_HOST"
   echo_color 3 "#### 增量备份流程已完成。"
 }
 
 proc_restore() {
   echo_color 3 "\n开始执行增量恢复流程..."
   list_snapshots "$ES_TARGET_HOST"
+  check_and_delete_target_index "$ES_TARGET_HOST"
   perform_restore "$ES_TARGET_HOST"
   wait_for_restore_completion "$ES_TARGET_HOST"
-  copy_temp_index_to_target_index "$ES_TARGET_HOST"
-  wait_for_reindex_completion "$ES_TARGET_HOST" "$TARGET_REINDEX_TASK_ID"
-  delete_temp_index "$ES_TARGET_HOST"
   echo_color 3 "#### 增量恢复流程已完成。"
 }
 
@@ -477,7 +393,6 @@ main() {
   # snapshot and restore procedure
   if [ "$BACKUP_MODE" == "1" ]; then
     proc_snapshot
-    check_if_restore
     proc_restore
   fi
 
